5b610462dc51ccb7bd9edb92c0ddb4f3a3bc054c
Mauro <mauro.bonfietti@gmail.com>
2017-02-07 13:35:02 -0300

    Nuevos tests con phpunit, de la Api Rest.

--- api-rest-slimphp/tests/Functional/ApiRestTest.php
+++ api-rest-slimphp/tests/Functional/ApiRestTest.php
@@ -19,16 +19,68 @@ class ApiRestTest extends BaseTestCase
         $this->assertNotContains('Failed', (string) $response->getBody());
     }
 
-    public function testGetUser()
+    public function testGetUsers()
     {
-        $response = $this->runApp('GET', '/users/1');
-
-        //print_r((string) $response->getBody());
+        $response = $this->runApp('GET', '/users');
 
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertContains('id', (string) $response->getBody());
         $this->assertContains('name', (string) $response->getBody());
         $this->assertContains('Juan', (string) $response->getBody());
+        $this->assertNotContains('error', (string) $response->getBody());
+    }
+
+    public function testGetUser()
+    {
+        $response = $this->runApp('GET', '/users/1');
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertContains('id', (string) $response->getBody());
+        $this->assertContains('name', (string) $response->getBody());
+        $this->assertContains('Juan', (string) $response->getBody());
+        $this->assertNotContains('error', (string) $response->getBody());
+    }
+
+    public function testSearchUsers()
+    {
+        $response = $this->runApp('GET', '/users/search/j');
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertContains('id', (string) $response->getBody());
+        $this->assertContains('name', (string) $response->getBody());
+        $this->assertContains('juan', (string) $response->getBody());
+        $this->assertNotContains('error', (string) $response->getBody());
+    }
+
+    public function testCreateUser()
+    {
+        $response = $this->runApp('POST', '/users', array('name' => 'Esteban'));
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertContains('id', (string) $response->getBody());
+        $this->assertContains('name', (string) $response->getBody());
+        $this->assertContains('Esteban', (string) $response->getBody());
+        $this->assertNotContains('error', (string) $response->getBody());
+    }
+
+    public function testUpdateUser()
+    {
+        $response = $this->runApp('PUT', '/users/4', array('name' => 'Tommy'));
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertContains('id', (string) $response->getBody());
+        $this->assertContains('name', (string) $response->getBody());
+        $this->assertContains('Tommy', (string) $response->getBody());
+        $this->assertNotContains('error', (string) $response->getBody());
+    }
+
+    public function testDeleteUser()
+    {
+        $response = $this->runApp('DELETE', '/users/3', array('name' => 'Tommy'));
+
+        //print_r((string) $response->getBody());
+
+        $this->assertEquals(200, $response->getStatusCode());
         $this->assertNotContains('error', (string) $response->getBody());
     }
 }
