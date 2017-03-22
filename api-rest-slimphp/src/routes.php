7758ae9ec15c517fec9966fbc29fd375ff739c10
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2017-03-22 20:57:57 -0300

    Agrego grupo de ruteo para usuarios y tareas.

--- api-rest-slimphp/src/routes.php
+++ api-rest-slimphp/src/routes.php
@@ -16,74 +16,86 @@ $app->get('/version', function () {
     return $this->response->withJson($msg);
 });
 
-$app->get('/tasks', function () {
-    $todos = tasks::getTasks($this->db);
+/**
+ * Routes Groups: Tasks.
+ */
+$app->group('/tasks', function () use ($app) {
 
-    return $this->response->withJson($todos);
+    $app->get('', function () {
+        $todos = tasks::getTasks($this->db);
+
+        return $this->response->withJson($todos);
+    });
+
+    $app->get('/[{id}]', function ($request, $response, $args) {
+        $todos = tasks::getTask($this->db, $args['id']);
+
+        return $this->response->withJson($todos);
+    });
+    
+    $app->get('/search/[{query}]', function ($request, $response, $args) {
+        $todos = tasks::searchTasks($this->db, $args['query']);
+
+        return $this->response->withJson($todos);
+    });
+    
+    $app->post('', function ($request) {
+        $input = tasks::createTask($this->db, $request);
+
+        return $this->response->withJson($input);
+    });
+
+    $app->put('/[{id}]', function ($request, $response, $args) {
+        $input = tasks::updateTask($this->db, $request, $args['id']);
+
+        return $this->response->withJson($input);
+    });
+
+    $app->delete('/[{id}]', function ($request, $response, $args) {
+        tasks::deleteTask($this->db, $args['id']);
+
+        return true;
+    });
 });
 
-$app->get('/tasks/[{id}]', function ($request, $response, $args) {
-    $todos = tasks::getTask($this->db, $args['id']);
+/**
+ * Routes Groups: Users.
+ */
+$app->group('/users', function () use ($app) {
 
-    return $this->response->withJson($todos);
-});
+    $app->get('', function () {
+        $result = users::getUsers($this->db);
 
-$app->get('/tasks/search/[{query}]', function ($request, $response, $args) {
-    $todos = tasks::searchTasks($this->db, $args['query']);
+        return $this->response->withJson($result, $result['code']);
+    });
 
-    return $this->response->withJson($todos);
-});
+    $app->get('/[{id}]', function ($request, $response, $args) {
+        $result = users::getUser($this->db, $args['id']);
 
-$app->post('/tasks', function ($request) {
-    $input = tasks::createTask($this->db, $request);
+        return $this->response->withJson($result, $result['code']);
+    });
 
-    return $this->response->withJson($input);
-});
+    $app->get('/search/[{query}]', function ($request, $response, $args) {
+        $result = users::searchUsers($this->db, $args['query']);
 
-$app->put('/tasks/[{id}]', function ($request, $response, $args) {
-    $input = tasks::updateTask($this->db, $request, $args['id']);
+        return $this->response->withJson($result, $result['code']);
+    });
 
-    return $this->response->withJson($input);
-});
+    $app->post('', function ($request) {
+        $result = users::createUser($this->db, $request);
 
-$app->delete('/tasks/[{id}]', function ($request, $response, $args) {
-    tasks::deleteTask($this->db, $args['id']);
+        return $this->response->withJson($result, $result['code']);
+    });
 
-    return true;
-});
+    $app->put('/[{id}]', function ($request, $response, $args) {
+        $result = users::updateUser($this->db, $request, $args['id']);
 
-$app->get('/users', function () {
-    $result = users::getUsers($this->db);
+        return $this->response->withJson($result, $result['code']);
+    });
 
-    return $this->response->withJson($result, $result['code']);
-});
+    $app->delete('/[{id}]', function ($request, $response, $args) {
+        $result = users::deleteUser($this->db, $args['id']);
 
-$app->get('/users/[{id}]', function ($request, $response, $args) {
-    $result = users::getUser($this->db, $args['id']);
-
-    return $this->response->withJson($result, $result['code']);
-});
-
-$app->get('/users/search/[{query}]', function ($request, $response, $args) {
-    $result = users::searchUsers($this->db, $args['query']);
-
-    return $this->response->withJson($result, $result['code']);
-});
-
-$app->post('/users', function ($request) {
-    $result = users::createUser($this->db, $request);
-
-    return $this->response->withJson($result, $result['code']);
-});
-
-$app->put('/users/[{id}]', function ($request, $response, $args) {
-    $result = users::updateUser($this->db, $request, $args['id']);
-
-    return $this->response->withJson($result, $result['code']);
-});
-
-$app->delete('/users/[{id}]', function ($request, $response, $args) {
-    $result = users::deleteUser($this->db, $args['id']);
-
-    return $this->response->withJson($result, $result['code']);
+        return $this->response->withJson($result, $result['code']);
+    });
 });
