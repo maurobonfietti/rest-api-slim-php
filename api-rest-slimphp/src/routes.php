7c4cf1814bd500524f4ea915984556d3d0233a8b
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2017-03-22 20:59:43 -0300

    + php-cs-fixer.

--- api-rest-slimphp/src/routes.php
+++ api-rest-slimphp/src/routes.php
@@ -20,37 +20,31 @@ $app->get('/version', function () {
  * Routes Groups: Tasks.
  */
 $app->group('/tasks', function () use ($app) {
-
     $app->get('', function () {
         $todos = tasks::getTasks($this->db);
 
         return $this->response->withJson($todos);
     });
-
     $app->get('/[{id}]', function ($request, $response, $args) {
         $todos = tasks::getTask($this->db, $args['id']);
 
         return $this->response->withJson($todos);
     });
-    
     $app->get('/search/[{query}]', function ($request, $response, $args) {
         $todos = tasks::searchTasks($this->db, $args['query']);
 
         return $this->response->withJson($todos);
     });
-    
     $app->post('', function ($request) {
         $input = tasks::createTask($this->db, $request);
 
         return $this->response->withJson($input);
     });
-
     $app->put('/[{id}]', function ($request, $response, $args) {
         $input = tasks::updateTask($this->db, $request, $args['id']);
 
         return $this->response->withJson($input);
     });
-
     $app->delete('/[{id}]', function ($request, $response, $args) {
         tasks::deleteTask($this->db, $args['id']);
 
@@ -62,37 +56,31 @@ $app->group('/tasks', function () use ($app) {
  * Routes Groups: Users.
  */
 $app->group('/users', function () use ($app) {
-
     $app->get('', function () {
         $result = users::getUsers($this->db);
 
         return $this->response->withJson($result, $result['code']);
     });
-
     $app->get('/[{id}]', function ($request, $response, $args) {
         $result = users::getUser($this->db, $args['id']);
 
         return $this->response->withJson($result, $result['code']);
     });
-
     $app->get('/search/[{query}]', function ($request, $response, $args) {
         $result = users::searchUsers($this->db, $args['query']);
 
         return $this->response->withJson($result, $result['code']);
     });
-
     $app->post('', function ($request) {
         $result = users::createUser($this->db, $request);
 
         return $this->response->withJson($result, $result['code']);
     });
-
     $app->put('/[{id}]', function ($request, $response, $args) {
         $result = users::updateUser($this->db, $request, $args['id']);
 
         return $this->response->withJson($result, $result['code']);
     });
-
     $app->delete('/[{id}]', function ($request, $response, $args) {
         $result = users::deleteUser($this->db, $args['id']);
 
