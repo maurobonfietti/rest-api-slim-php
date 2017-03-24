1634831212e2eb8350474cb5a2a21a7f5d2b45ce
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2017-03-24 03:16:30 -0300

    Muevo los mensajes de usuarios y tareas en la clase base.

--- api-rest-slimphp/src/tasks.php
+++ api-rest-slimphp/src/tasks.php
@@ -2,14 +2,6 @@
 
 class tasks extends base
 {
-    const TASK_NOT_FOUND = 'La tarea solicitada no existe.';
-
-    const TASK_NAME_NOT_FOUND = 'No se encontraron tareas con ese nombre.';
-
-    const TASK_NAME_REQUIRED = 'Ingrese el nombre de la tarea.';
-
-    const TASK_DELETED = 'La tarea fue eliminada correctamente.';
-
     /**
      * Check if the task exists.
      *
