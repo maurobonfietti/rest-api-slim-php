902d9777b8b73118afa42df5ddc885fe0dd830c6
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2017-04-03 22:13:13 -0300

    Probando formato de respuesta/salida con JSON_PRETTY_PRINT.

--- api-rest-slimphp/src/routes.php
+++ api-rest-slimphp/src/routes.php
@@ -9,7 +9,7 @@ $app->get('/', function () {
         'users' => 'Ver Usuarios: /users',
         'version' => 'Ver Version: /version',
     ]];
-    return $this->response->withJson($msg);
+    return $this->response->withJson($msg, 200, JSON_PRETTY_PRINT);
 });
 
 /**
@@ -17,7 +17,7 @@ $app->get('/', function () {
  */
 $app->get('/version', function () {
     $msg = ['info' => ['api_version' => '0.1.15 [03/04/2017]']];
-    return $this->response->withJson($msg);
+    return $this->response->withJson($msg, 200, JSON_PRETTY_PRINT);
 });
 
 /**
@@ -26,27 +26,27 @@ $app->get('/version', function () {
 $app->group('/tasks', function () use ($app) {
     $app->get('', function () {
         $result = TasksController::getTasks($this->db);
-        return $this->response->withJson($result, $result['code']);
+        return $this->response->withJson($result, $result['code'], JSON_PRETTY_PRINT);
     });
     $app->get('/[{id}]', function ($request, $response, $args) {
         $result = TasksController::getTask($this->db, $args['id']);
-        return $this->response->withJson($result, $result['code']);
+        return $this->response->withJson($result, $result['code'], JSON_PRETTY_PRINT);
     });
     $app->get('/search/[{query}]', function ($request, $response, $args) {
         $result = TasksController::searchTasks($this->db, $args['query']);
-        return $this->response->withJson($result, $result['code']);
+        return $this->response->withJson($result, $result['code'], JSON_PRETTY_PRINT);
     });
     $app->post('', function ($request) {
         $result = TasksController::createTask($this->db, $request);
-        return $this->response->withJson($result, $result['code']);
+        return $this->response->withJson($result, $result['code'], JSON_PRETTY_PRINT);
     });
     $app->put('/[{id}]', function ($request, $response, $args) {
         $result = TasksController::updateTask($this->db, $request, $args['id']);
-        return $this->response->withJson($result, $result['code']);
+        return $this->response->withJson($result, $result['code'], JSON_PRETTY_PRINT);
     });
     $app->delete('/[{id}]', function ($request, $response, $args) {
         $result = TasksController::deleteTask($this->db, $args['id']);
-        return $this->response->withJson($result, $result['code']);
+        return $this->response->withJson($result, $result['code'], JSON_PRETTY_PRINT);
     });
 });
 
@@ -56,26 +56,26 @@ $app->group('/tasks', function () use ($app) {
 $app->group('/users', function () use ($app) {
     $app->get('', function () {
         $result = UsersController::getUsers($this->db);
-        return $this->response->withJson($result, $result['code']);
+        return $this->response->withJson($result, $result['code'], JSON_PRETTY_PRINT);
     });
     $app->get('/[{id}]', function ($request, $response, $args) {
         $result = UsersController::getUser($this->db, $args['id']);
-        return $this->response->withJson($result, $result['code']);
+        return $this->response->withJson($result, $result['code'], JSON_PRETTY_PRINT);
     });
     $app->get('/search/[{query}]', function ($request, $response, $args) {
         $result = UsersController::searchUsers($this->db, $args['query']);
-        return $this->response->withJson($result, $result['code']);
+        return $this->response->withJson($result, $result['code'], JSON_PRETTY_PRINT);
     });
     $app->post('', function ($request) {
         $result = UsersController::createUser($this->db, $request);
-        return $this->response->withJson($result, $result['code']);
+        return $this->response->withJson($result, $result['code'], JSON_PRETTY_PRINT);
     });
     $app->put('/[{id}]', function ($request, $response, $args) {
         $result = UsersController::updateUser($this->db, $request, $args['id']);
-        return $this->response->withJson($result, $result['code']);
+        return $this->response->withJson($result, $result['code'], JSON_PRETTY_PRINT);
     });
     $app->delete('/[{id}]', function ($request, $response, $args) {
         $result = UsersController::deleteUser($this->db, $args['id']);
-        return $this->response->withJson($result, $result['code']);
+        return $this->response->withJson($result, $result['code'], JSON_PRETTY_PRINT);
     });
 });
