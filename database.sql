a14053259615b36b35e37a2dace1187223ceb5e2
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2017-04-10 22:56:15 -0300

    BD: Unifico estructura y datos en archivo "database.sql".

---
+++ database.sql
@@ -0,0 +1,37 @@
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Table structure for `tasks`
+--
+DROP TABLE IF EXISTS `tasks`;
+CREATE TABLE IF NOT EXISTS `tasks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `task` varchar(200) NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '0',
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+INSERT INTO `users` (`id`, `name`, `email`) VALUES ('1', 'Juan', 'juanmartin@delpotro.com');
+INSERT INTO `users` (`id`, `name`, `email`) VALUES ('2', 'Federico', null);
+INSERT INTO `users` (`id`, `name`, `email`) VALUES ('3', 'Leo', null);
+INSERT INTO `users` (`id`, `name`, `email`) VALUES ('4', 'Carlos', null);
+INSERT INTO `users` (`id`, `name`, `email`) VALUES ('5', 'Diego', 'diego10@gmail.com');
+
+INSERT INTO `tasks` (`id`, `task`, `status`) VALUES (1, 'Ir al centro', 1);
+INSERT INTO `tasks` (`id`, `task`, `status`) VALUES (2, 'Comprar zapatillas', 1);
+INSERT INTO `tasks` (`id`, `task`, `status`) VALUES (3, 'Ir al super', 1);
+INSERT INTO `tasks` (`id`, `task`, `status`) VALUES (4, 'Comprar cereales', 1);
+INSERT INTO `tasks` (`id`, `task`, `status`) VALUES (5, 'Hacer tarea...', 0);
