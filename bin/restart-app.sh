7b33f0071b422e2b0cf0a9d2d46dfee49a6f0645
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2017-04-13 15:10:46 -0300

    Retoque menor en restart-app.sh

--- bin/restart-app.sh
+++ bin/restart-app.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mysql -uroot -p -e 'DROP DATABASE api_rest_slimphp ; CREATE DATABASE api_rest_slimphp' 2> /dev/null
+mysql -uroot -p -e 'DROP DATABASE IF EXITS api_rest_slimphp ; CREATE DATABASE api_rest_slimphp' 2> /dev/null
 mysql -uroot -p api_rest_slimphp < data/database.sql 2> /dev/null
 ./bin/generate-doc.sh > DOC.md 2> /dev/null
