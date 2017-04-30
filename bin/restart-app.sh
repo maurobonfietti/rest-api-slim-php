e8bb656da1797c76afa420e43a503ec722019c7f
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2017-04-30 04:49:37 -0300

    Ejecuto unit test luego del restart app.

--- bin/restart-app.sh
+++ bin/restart-app.sh
@@ -3,3 +3,4 @@
 mysql -uroot -p -e 'DROP DATABASE IF EXITS api_rest_slimphp ; CREATE DATABASE api_rest_slimphp' 2> /dev/null
 mysql -uroot -p api_rest_slimphp < app/data/database.sql 2> /dev/null
 ./bin/generate-doc.sh > DOC.md 2> /dev/null
+./vendor/bin/phpunit
