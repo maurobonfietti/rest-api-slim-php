77f97299b03241f047bee39bbd0bf0bdf598421a
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2017-05-01 15:42:42 -0300

    Mejorando el logeo de cada request/response realizado en la api.

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -25,13 +25,41 @@ abstract class BaseController
         $this->request = $request;
         $this->response = $response;
         $this->args = $args;
-        $info = [
-            'method' => $this->request->getAttribute('route')->getMethods(),
-            'path' => $this->request->getUri()->getPath(),
-            'input' => $this->request->getParsedBody(),
-            'args' => $this->args,
-        ];
-        $this->logger->info(json_encode($info));
+        $this->logRequest();
+    }
+
+    protected function logRequest()
+    {
+//        $info = [
+//            'method' => $this->request->getAttribute('route')->getMethods(),
+//            'path' => $this->request->getUri()->getPath(),
+//            'input' => $this->request->getParsedBody(),
+//            'args' => $this->args,
+//        ];
+        
+
+        $routeInfo = $this->request->getAttribute('routeInfo');
+        $route = $this->request->getAttribute('route');
+        
+        $this->logger->info('**********');
+        $this->logger->info('* REQUEST: ' . $route->getCallable()[1]);
+        $this->logger->info('* ' . $route->getMethods()[0] . ' ' . $routeInfo['request'][1]);
+        $this->logger->info('* BODY: ' . json_encode($this->request->getParsedBody()));
+        $this->logger->info('* ARGS: ' . json_encode($this->args));
+
+//        $routeName = $route->getName();
+//        $groups = $route->getGroups();
+//        $methods = $route->getMethods();
+//        $arguments = $route->getArguments();
+//
+//        var_dump($route->getCallable()[1]);
+//        print "Route Info: " . print_r($route, true);
+//        print "Route Name: " . print_r($routeName, true);
+//        print "Route Groups: " . print_r($groups, true);
+//        print "Route Methods: " . print_r($methods, true);
+//        print "Route Arguments: " . print_r($arguments, true);
+//        exit;
+//        $this->logger->info(json_encode($info));
     }
 
     /**
@@ -49,7 +77,9 @@ abstract class BaseController
             'status' => $status,
             'message' => $message,
         ];
-        $this->logger->info(json_encode($result));
+//        $this->logger->info(json_encode($result));
+        $this->logger->info('* RESPONSE: ' . json_encode($result));
+        $this->logger->info('**********');
 
         return $this->response->withJson($result, $code, JSON_PRETTY_PRINT);
     }
