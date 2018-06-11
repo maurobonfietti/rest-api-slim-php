506dfa93b33aba02f1e72361479aa3dfe78d26ae
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-06-11 00:34:42 -0300

    Show exception code in ApiError.

--- src/Handlers/ApiError.php
+++ src/Handlers/ApiError.php
@@ -9,14 +9,18 @@ final class ApiError extends \Slim\Handlers\Error
 {
     public function __invoke(Request $request, Response $response, \Exception $exception)
     {
-        $status = $exception->getCode() ?: 500;
+        $statusCode = $exception->getCode() ?: 500;
+        if ($statusCode > 512) {
+            $statusCode = 500;
+        }
         $data = [
-            "status" => "error",
-            "message" => $exception->getMessage(),
+            'code' => $exception->getCode(),
+            'status' => 'error',
+            'message' => $exception->getMessage(),
         ];
         $body = json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
         return $response
-                    ->withStatus($status)
+                    ->withStatus($statusCode)
                     ->withHeader("Content-type", "application/json")
                     ->write($body);
     }
