875303462f7a138e282a5262608f346ad2e115a7
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-06-11 22:37:35 -0300

    Minor change.

--- app/app.php
+++ app/app.php
@@ -1,7 +1,7 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
 define('ROOT_PATH', __DIR__ . '/../');
+require __DIR__ . '/../vendor/autoload.php';
 $envFile = ROOT_PATH . '.env';
 if (file_exists($envFile)) {
     $dotenv = new Dotenv\Dotenv(ROOT_PATH);
