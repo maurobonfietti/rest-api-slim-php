d512757f76bff2901510d801fa6ca36f4d92ef09
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-08-11 02:27:56 -0300

    Minor changes in default database.

--- app/data/database.sql
+++ app/data/database.sql
@@ -1,6 +1,7 @@
---
--- Table structure for `tasks`
---
+
+-- ----------------------------
+-- Table structure for tasks
+-- ----------------------------
 DROP TABLE IF EXISTS `tasks`;
 CREATE TABLE IF NOT EXISTS `tasks` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -25,8 +26,8 @@ INSERT INTO `tasks` (`id`, `name`, `status`) VALUES (5, 'Do homework...', 0);
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8_unicode_ci,
+  `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8_unicode_ci,
   `updated` timestamp DEFAULT NOW() ON UPDATE NOW() ,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
@@ -35,7 +36,7 @@ CREATE TABLE `users` (
 -- Records of users
 -- ----------------------------
 INSERT INTO `users` (`id`, `name`, `email`) VALUES ('1', 'Juan', 'juanmartin@mail.com');
-INSERT INTO `users` (`id`, `name`, `email`) VALUES ('2', 'Federico', null);
-INSERT INTO `users` (`id`, `name`, `email`) VALUES ('3', 'Leo', null);
+INSERT INTO `users` (`id`, `name`, `email`) VALUES ('2', 'James', null);
+INSERT INTO `users` (`id`, `name`, `email`) VALUES ('3', 'Lionel', null);
 INSERT INTO `users` (`id`, `name`, `email`) VALUES ('4', 'Carlos', null);
 INSERT INTO `users` (`id`, `name`, `email`) VALUES ('5', 'Diego', 'diego1010@gmail.com');
