3e26d10d5c4a7467a07c2509607ae80b66e40902
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-08-11 02:22:24 -0300

    Minor changes DB Fixture.

--- app/data/database.sql
+++ app/data/database.sql
@@ -13,11 +13,11 @@ CREATE TABLE IF NOT EXISTS `tasks` (
 -- ----------------------------
 -- Records of tasks
 -- ----------------------------
-INSERT INTO `tasks` (`id`, `name`, `status`) VALUES (1, 'Ir al centro', 1);
-INSERT INTO `tasks` (`id`, `name`, `status`) VALUES (2, 'Comprar zapatillas', 1);
-INSERT INTO `tasks` (`id`, `name`, `status`) VALUES (3, 'Ir al super', 1);
-INSERT INTO `tasks` (`id`, `name`, `status`) VALUES (4, 'Comprar cereales', 1);
-INSERT INTO `tasks` (`id`, `name`, `status`) VALUES (5, 'Hacer tarea...', 0);
+INSERT INTO `tasks` (`id`, `name`, `status`) VALUES (1, 'Go to cinema', 1);
+INSERT INTO `tasks` (`id`, `name`, `status`) VALUES (2, 'Buying shoes', 1);
+INSERT INTO `tasks` (`id`, `name`, `status`) VALUES (3, 'Go to shopping', 1);
+INSERT INTO `tasks` (`id`, `name`, `status`) VALUES (4, 'Buy milk', 1);
+INSERT INTO `tasks` (`id`, `name`, `status`) VALUES (5, 'Do homework...', 0);
 
 -- ----------------------------
 -- Table structure for users
@@ -34,8 +34,8 @@ CREATE TABLE `users` (
 -- ----------------------------
 -- Records of users
 -- ----------------------------
-INSERT INTO `users` (`id`, `name`, `email`) VALUES ('1', 'Juan', 'juanmartin@delpotro.com');
+INSERT INTO `users` (`id`, `name`, `email`) VALUES ('1', 'Juan', 'juanmartin@mail.com');
 INSERT INTO `users` (`id`, `name`, `email`) VALUES ('2', 'Federico', null);
 INSERT INTO `users` (`id`, `name`, `email`) VALUES ('3', 'Leo', null);
 INSERT INTO `users` (`id`, `name`, `email`) VALUES ('4', 'Carlos', null);
-INSERT INTO `users` (`id`, `name`, `email`) VALUES ('5', 'Diego', 'diego10@gmail.com');
+INSERT INTO `users` (`id`, `name`, `email`) VALUES ('5', 'Diego', 'diego1010@gmail.com');
