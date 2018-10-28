3ba6e44438fbfd1cf4a9a49471f41de8f379e511
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-10-28 03:25:59 -0300

    Go to sleep man.

--- src/Exception/TaskException.php
+++ src/Exception/TaskException.php
@@ -8,11 +8,11 @@ namespace App\Exception;
 class TaskException extends BaseException
 {
     const TASK_NOT_FOUND = 'Task not found.';
-    const TASK_NAME_NOT_FOUND = 'Task name not found.';
-    const TASK_NAME_REQUIRED = 'The field "name" is required.';
+    const TASK_NAME_NOT_FOUND = 'Task not found.';
+    const TASK_NAME_REQUIRED = 'Fields required.';
     const TASK_INFO_REQUIRED = 'Enter the data to update the task.';
-    const TASK_NAME_INVALID = 'Invalid name.';
-    const TASK_STATUS_INVALID = 'Invalid status';
+    const TASK_NAME_INVALID = 'Error in task.';
+    const TASK_STATUS_INVALID = 'Error in status.';
 
     /**
      * @param string $message
--- src/Exception/UserException.php
+++ src/Exception/UserException.php
@@ -8,11 +8,11 @@ namespace App\Exception;
 class UserException extends BaseException
 {
     const USER_NOT_FOUND = 'User not found.';
-    const USER_NAME_NOT_FOUND = 'User name not found';
-    const USER_NAME_REQUIRED = 'The field "name" is required.';
+    const USER_NAME_NOT_FOUND = 'User not found';
+    const USER_NAME_REQUIRED = 'Fields required.';
     const USER_INFO_REQUIRED = 'Enter the data to update the user.';
-    const USER_NAME_INVALID = 'Invalid name.';
-    const USER_EMAIL_INVALID = 'Invalid email';
+    const USER_NAME_INVALID = 'Error in user.';
+    const USER_EMAIL_INVALID = 'Error email.';
 
     /**
      * @param string $message
