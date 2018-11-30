9fae025da4b07256d94b461a0be54f03720ca607
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-11-30 20:42:48 -0300

    Fix phpunit test.

--- tests/api/DefaultTest.php
+++ tests/api/DefaultTest.php
@@ -18,22 +18,6 @@ class DefaultTest extends BaseTestCase
     }
 
     /**
-     * Test that version endpoint, show the api version.
-     */
-    public function testVersion()
-    {
-        $response = $this->runApp('GET', '/version');
-
-        //print_r((string) $response->getBody());
-
-        $this->assertEquals(200, $response->getStatusCode());
-        $this->assertContains('version', (string) $response->getBody());
-        $this->assertNotContains('ERROR', (string) $response->getBody());
-        $this->assertNotContains('Failed', (string) $response->getBody());
-    }
-
-
-    /**
      * Test that status endpoint, show the api status ok.
      */
     public function testStatus()
