b3427de70039547782ec15899d4b27e8dd000100
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2019-03-24 19:13:06 -0300

    Update PHPUnit Test when Login Failed.

--- tests/api/DefaultTest.php
+++ tests/api/DefaultTest.php
@@ -2,8 +2,6 @@
 
 namespace Tests\api;
 
-use App\Exception\UserException;
-
 class DefaultTest extends BaseTestCase
 {
     /**
@@ -68,35 +66,15 @@ class DefaultTest extends BaseTestCase
      */
     public function testLoginFailed()
     {
-//        $this->expectException(\App\Exception\UserException::class);
         $response = $this->runApp('POST', '/login', ['email' => 'a@b.com', 'password' => 'p']);
 
         $result = (string) $response->getBody();
-        
+
         $this->assertStringContainsString('Login failed', $result);
         $this->assertStringContainsString('UserException', $result);
         $this->assertStringContainsString('error', $result);
         $this->assertStringNotContainsString('success', $result);
         $this->assertStringNotContainsString('Authorization', $result);
         $this->assertStringNotContainsString('Bearer', $result);
-
-//        var_dump($result);
-//        exit;
-//        $this->setExpectedException(Aivo\Exceptions\TagException\TitleRequiredException::class);
-//        $this->expectException(\App\Exception\UserException::class);
-//        $this->expectException(UserException::class);
-//        $this->expectException(InvalidArgumentException::class);
-
-//        self::$jwt = json_decode($result)->message->Authorization;
-//
-//        $this->assertEquals(200, $response->getStatusCode());
-//        $this->assertStringContainsString('status', $result);
-//        $this->assertStringContainsString('success', $result);
-//        $this->assertStringContainsString('message', $result);
-//        $this->assertStringContainsString('Authorization', $result);
-//        $this->assertStringContainsString('Bearer', $result);
-//        $this->assertStringContainsString('ey', $result);
-//        $this->assertStringNotContainsString('ERROR', $result);
-//        $this->assertStringNotContainsString('Failed', $result);
     }
 }
