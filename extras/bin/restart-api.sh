d2974ab4758886cdd2fbe28ec81a9cbafd005f4a
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2019-06-20 22:32:20 -0300

    Update script to restart database.

--- extras/bin/restart-api.sh
+++ extras/bin/restart-api.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo -e "Reading environment variables..."
+echo -e "# Read environment variables."
 source .env
 
-echo -e "Restarting demo database..."
+echo -e "# Restart demo database."
 mysql -u$DB_USERNAME -p$DB_PASSWORD -h$DB_HOSTNAME -e "DROP DATABASE IF EXISTS $DB_DATABASE ; CREATE DATABASE $DB_DATABASE" 2> /dev/null
 
-echo -e "Creating testing data..."
+echo -e "# Create testing data."
 mysql -u$DB_USERNAME -p$DB_PASSWORD -h$DB_HOSTNAME $DB_DATABASE < database/database.sql 2> /dev/null
 
-echo -e "Running unit tests..."
+echo -e "# Run tests."
 ./vendor/bin/phpunit
