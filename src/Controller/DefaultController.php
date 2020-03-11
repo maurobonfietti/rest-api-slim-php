6b7068e0a0f1344762ff5d0341a2dcaa909f9d24
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2020-03-11 18:43:06 -0300

    Update API version.

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -8,7 +8,7 @@ use Slim\Http\Response;
 
 class DefaultController extends BaseController
 {
-    const API_VERSION = '0.37.0';
+    const API_VERSION = '0.38.0';
 
     public function __construct(Container $container)
     {
