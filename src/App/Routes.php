321c1d2e4e7918437dbb3ba3de2da8a06e4c5a02
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2020-04-10 01:25:28 -0300

    Minor changes when declare(strict_types=1);

--- src/App/Routes.php
+++ src/App/Routes.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 $app->get('/', 'App\Controller\DefaultController:getHelp');
 $app->get('/status', 'App\Controller\DefaultController:getStatus');
