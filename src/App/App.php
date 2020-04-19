e483f69d6a00a8a344f5bd04f74fd4cba3a3c352
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2020-04-19 01:48:19 -0300

    Set env vars of MySQL connection as required.

--- src/App/App.php
+++ src/App/App.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 
 require __DIR__ . '/../../vendor/autoload.php';
 $baseDir = __DIR__ . '/../../';
+$dotenv = new Dotenv\Dotenv($baseDir);
 $envFile = $baseDir . '.env';
 if (file_exists($envFile)) {
-    $dotenv = new Dotenv\Dotenv($baseDir);
     $dotenv->load();
 }
+$dotenv->required(['DB_HOSTNAME', 'DB_DATABASE', 'DB_USERNAME', 'DB_PASSWORD']);
 $settings = require __DIR__ . '/Settings.php';
 $app = new \Slim\App($settings);
 require __DIR__ . '/Dependencies.php';
