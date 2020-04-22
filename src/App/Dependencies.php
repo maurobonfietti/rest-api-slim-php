e389901e50ef3f2b29eba430e0914a8fe9ad4500
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2020-04-21 23:27:54 -0300

    Fix issues detected with PHP Insights.

--- src/App/Dependencies.php
+++ src/App/Dependencies.php
@@ -10,8 +10,11 @@ $container = $app->getContainer();
 
 $container['db'] = static function (ContainerInterface $c): PDO {
     $db = $c->get('settings')['db'];
-    $database = sprintf('mysql:host=%s;dbname=%s', $db['hostname'], $db['database']);
-    $pdo = new PDO($database, $db['username'], $db['password']);
+    $pdo = new PDO(
+        sprintf('mysql:host=%s;dbname=%s', $db['hostname'], $db['database']),
+        $db['username'],
+        $db['password']
+    );
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
--- src/Middleware/Base.php
+++ src/Middleware/Base.php
@@ -25,4 +25,3 @@ abstract class Base
         }
     }
 }
-
