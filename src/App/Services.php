45b88ec2c0dcd644735016dc7eedb7a718062240
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2020-06-11 18:19:11 -0300

    Minor changes.

--- src/App/Services.php
+++ src/App/Services.php
@@ -9,8 +9,14 @@ use Psr\Container\ContainerInterface;
 
 $container = $app->getContainer();
 
-$container['user_service'] = static fn (ContainerInterface $container): UserService => new UserService($container->get('user_repository'), $container->get('redis_service'));
+$container['user_service'] = static function (ContainerInterface $container): UserService {
+    return new UserService($container->get('user_repository'), $container->get('redis_service'));
+};
 
-$container['task_service'] = static fn (ContainerInterface $container): TaskService => new TaskService($container->get('task_repository'), $container->get('redis_service'));
+$container['task_service'] = static function (ContainerInterface $container): TaskService {
+    return new TaskService($container->get('task_repository'), $container->get('redis_service'));
+};
 
-$container['note_service'] = static fn (ContainerInterface $container): NoteService => new NoteService($container->get('note_repository'), $container->get('redis_service'));
+$container['note_service'] = static function (ContainerInterface $container): NoteService {
+    return new NoteService($container->get('note_repository'), $container->get('redis_service'));
+};
