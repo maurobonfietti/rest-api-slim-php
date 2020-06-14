df43c0772d4da1422baede09e046d4a7e3511f4a
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2020-06-13 21:49:40 -0300

    Move env var for database connection settings.

--- src/App/Dependencies.php
+++ src/App/Dependencies.php
@@ -4,14 +4,12 @@ declare(strict_types=1);
 
 use App\Handler\ApiError;
 use App\Service\RedisService;
-use Psr\Container\ContainerInterface;
 
 $container = $app->getContainer();
 
-$container['db'] = static function (ContainerInterface $c): PDO {
-    $db = $c->get('settings')['db'];
-    $dsn = sprintf('mysql:host=%s;dbname=%s', $db['hostname'], $db['database']);
-    $pdo = new PDO($dsn, $db['username'], $db['password']);
+$container['db'] = static function (): PDO {
+    $dsn = sprintf('mysql:host=%s;dbname=%s', getenv('DB_HOSTNAME'), getenv('DB_DATABASE'));
+    $pdo = new PDO($dsn, getenv('DB_USERNAME'), getenv('DB_PASSWORD'));
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
