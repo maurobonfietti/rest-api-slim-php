106f179404626af3adb0130f51bfd35e61ef2d55
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2020-06-27 00:17:05 -0300

    Change some routes.

--- src/App/Routes.php
+++ src/App/Routes.php
@@ -14,28 +14,28 @@ $app->post('/login', \App\Controller\User\Login::class);
 $app->group('/api/v1', function () use ($app): void {
     $app->group('/tasks', function () use ($app): void {
         $app->get('', Task\GetAll::class);
-        $app->get('/[{id}]', Task\GetOne::class);
-        $app->get('/search/[{query}]', Task\Search::class);
         $app->post('', Task\Create::class);
-        $app->put('/[{id}]', Task\Update::class);
-        $app->delete('/[{id}]', Task\Delete::class);
+        $app->get('/search/[{query}]', Task\Search::class);
+        $app->get('/{id}', Task\GetOne::class);
+        $app->put('/{id}', Task\Update::class);
+        $app->delete('/{id}', Task\Delete::class);
     })->add(new Auth());
 
     $app->group('/users', function () use ($app): void {
         $app->get('', User\GetAll::class)->add(new Auth());
-        $app->get('/[{id}]', User\GetOne::class)->add(new Auth());
-        $app->get('/search/[{query}]', User\Search::class)->add(new Auth());
         $app->post('', User\Create::class);
-        $app->put('/[{id}]', User\Update::class)->add(new Auth());
-        $app->delete('/[{id}]', User\Delete::class)->add(new Auth());
+        $app->get('/search/[{query}]', User\Search::class)->add(new Auth());
+        $app->get('/{id}', User\GetOne::class)->add(new Auth());
+        $app->put('/{id}', User\Update::class)->add(new Auth());
+        $app->delete('/{id}', User\Delete::class)->add(new Auth());
     });
 
     $app->group('/notes', function () use ($app): void {
         $app->get('', Note\GetAll::class);
-        $app->get('/[{id}]', Note\GetOne::class);
-        $app->get('/search/[{query}]', Note\Search::class);
         $app->post('', Note\Create::class);
-        $app->put('/[{id}]', Note\Update::class);
-        $app->delete('/[{id}]', Note\Delete::class);
+        $app->get('/search/[{query}]', Note\Search::class);
+        $app->get('/{id}', Note\GetOne::class);
+        $app->put('/{id}', Note\Update::class);
+        $app->delete('/{id}', Note\Delete::class);
     });
 });
