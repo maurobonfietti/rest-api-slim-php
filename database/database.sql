54aadfc5b05805a47e629a900eac16347674f1a8
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2020-07-09 18:14:10 -0300

    Added some notes/quotes examples from: brainyquote.com

--- database/database.sql
+++ database/database.sql
@@ -69,4 +69,19 @@ INSERT INTO `notes` (`id`, `name`, `description`) VALUES ('3', 'Long Note 3', 'T
 INSERT INTO `notes` (`id`, `name`, `description`) VALUES ('4', 'Napoleon Hill', 'Whatever the mind of man can conceive and believe, it can achieve.');
 INSERT INTO `notes` (`id`, `name`, `description`) VALUES ('5', 'Note 5', 'A Random Note');
 
+INSERT INTO `notes`
+    (`name`, `description`)
+VALUES
+    ('Brian Tracy', 'Develop an attitude of gratitude, and give thanks for everything that happens to you, knowing that every step forward is a step toward achieving something bigger and better than your current situation.'),
+    ('Zig Ziglar', 'Your attitude, not your aptitude, will determine your altitude.'),
+    ('William James', 'The greatest discovery of my generation is that a human being can alter his life by altering his attitudes.'),
+    ('Og Mandino', 'Take the attitude of a student, never be too big to ask questions, never know too much to learn something new.'),
+    ('Earl Nightingale', 'Our attitude towards others determines their attitude towards us.'),
+    ('Norman Vincent Peale', 'Watch your manner of speech if you wish to develop a peaceful state of mind. Start each day by affirming peaceful, contented and happy attitudes and your days will tend to be pleasant and successful.'),
+    ('W. Clement Stone', 'There is little difference in people, but that little difference makes a big difference. The little difference is attitude. The big difference is whether it is positive or negative.'),
+    ('Dale Carnegie', 'Happiness does not depend on any external conditions, it is governed by our mental attitude.'),
+    ('Walt Disney', 'If you can dream it, you can do it.'),
+    ('William Shakespeare', 'Our doubts are traitors and make us lose the good we oft might win by fearing to attempt.'),
+    ('Albert Einstein', 'A person who never made a mistake never tried anything new.');
+
 SET FOREIGN_KEY_CHECKS = 1;
