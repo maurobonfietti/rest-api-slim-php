12e1eafb6c906a266c95ec28138fc31abdfc6132
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2020-07-09 19:57:00 -0300

    Validate page and perPage values.

--- src/Service/Note/Find.php
+++ src/Service/Note/Find.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Service\Note;
 
+use App\Exception\Note;
+
 final class Find extends Base
 {
     public function getAll(): array
@@ -13,6 +15,13 @@ final class Find extends Base
 
     public function getNotesByPage($page, $perPage): array
     {
+        if (! is_numeric($page) || $page < 1) {
+            throw new Note('Invalid page value.', 400);
+        }
+        if (! is_numeric($perPage) || $perPage < 1) {
+            throw new Note('Invalid perPage value.', 400);
+        }
+
         return $this->noteRepository->getNotesByPage($page, $perPage);
     }
 
