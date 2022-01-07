652372e524458da59d22254677bc45d532f17acc
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2022-01-07 17:51:49 -0300

    php-cs-fixer fix --rules @PSR12 src

--- src/App/Services.php
+++ src/App/Services.php
@@ -7,52 +7,72 @@ use App\Service\Task\TaskService;
 use App\Service\User;
 use Psr\Container\ContainerInterface;
 
-$container['find_user_service'] = static fn (ContainerInterface $container): User\Find => new User\Find(
+$container['find_user_service'] = static fn (
+    ContainerInterface $container
+): User\Find => new User\Find(
     $container->get('user_repository'),
     $container->get('redis_service')
 );
 
-$container['create_user_service'] = static fn (ContainerInterface $container): User\Create => new User\Create(
+$container['create_user_service'] = static fn (
+    ContainerInterface $container
+): User\Create => new User\Create(
     $container->get('user_repository'),
     $container->get('redis_service')
 );
 
-$container['update_user_service'] = static fn (ContainerInterface $container): User\Update => new User\Update(
+$container['update_user_service'] = static fn (
+    ContainerInterface $container
+): User\Update => new User\Update(
     $container->get('user_repository'),
     $container->get('redis_service')
 );
 
-$container['delete_user_service'] = static fn (ContainerInterface $container): User\Delete => new User\Delete(
+$container['delete_user_service'] = static fn (
+    ContainerInterface $container
+): User\Delete => new User\Delete(
     $container->get('user_repository'),
     $container->get('redis_service')
 );
 
-$container['login_user_service'] = static fn (ContainerInterface $container): User\Login => new User\Login(
+$container['login_user_service'] = static fn (
+    ContainerInterface $container
+): User\Login => new User\Login(
     $container->get('user_repository'),
     $container->get('redis_service')
 );
 
-$container['task_service'] = static fn (ContainerInterface $container): TaskService => new TaskService(
+$container['task_service'] = static fn (
+    ContainerInterface $container
+): TaskService => new TaskService(
     $container->get('task_repository'),
     $container->get('redis_service')
 );
 
-$container['find_note_service'] = static fn (ContainerInterface $container): Note\Find => new Note\Find(
+$container['find_note_service'] = static fn (
+    ContainerInterface $container
+): Note\Find => new Note\Find(
     $container->get('note_repository'),
     $container->get('redis_service')
 );
 
-$container['create_note_service'] = static fn (ContainerInterface $container): Note\Create => new Note\Create(
+$container['create_note_service'] = static fn (
+    ContainerInterface $container
+): Note\Create => new Note\Create(
     $container->get('note_repository'),
     $container->get('redis_service')
 );
 
-$container['update_note_service'] = static fn (ContainerInterface $container): Note\Update => new Note\Update(
+$container['update_note_service'] = static fn (
+    ContainerInterface $container
+): Note\Update => new Note\Update(
     $container->get('note_repository'),
     $container->get('redis_service')
 );
 
-$container['delete_note_service'] = static fn (ContainerInterface $container): Note\Delete => new Note\Delete(
+$container['delete_note_service'] = static fn (
+    ContainerInterface $container
+): Note\Delete => new Note\Delete(
     $container->get('note_repository'),
     $container->get('redis_service')
 );
